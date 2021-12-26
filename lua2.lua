d2039d4b77bd9d0b986a49f8ba7bdb41932d945e
xyw <xuyiwen2021@foxmail.com>
2021-12-26 20:21:59 +0800

    12-26

--- lua2.lua
+++ lua2.lua
@@ -1,7 +1,11 @@
 require("lua3")
-require("path.hello")
+local hello =  require("path.hello")
 
-print(math.abs(-100));
+-- print(math.abs(-100));
 
-print(string.find("abcb","bc"));
+-- print(string.find("abcb","bc"));
 
+
+-- print(package.path)
+
+hello.say()
--- path/hello.lua
+++ path/hello.lua
@@ -1,1 +1,11 @@
 print("hello")
+
+local hello = {}
+
+
+function hello.say()
+    print("hello woshi hello")
+    
+end
+
+return hello
